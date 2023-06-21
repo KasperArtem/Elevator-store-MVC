c93b881525080ac7924afcede685b5a698c2c28c
KasperArtem <32073051+KasperArtem@users.noreply.github.com>
2023-06-21 16:47:06 +0300

    Elevator-store-MVC

---
+++ MVC_Store/App_Start/BundleConfig.cs
@@ -0,0 +1,37 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace MVC_Store
+{
+    public class BundleConfig
+    {
+        
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        "~/Scripts/jquery.validate*"));
+
+            
+            bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
+                        "~/Scripts/jquery-ui.js"));
+
+           
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/Scripts/bootstrap.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/bootstrap.css",
+                      "~/Content/site.css",
+                      "~/Content/jquery-ui.css"));
+
+            bundles.Add(new ScriptBundle("~/bundles/ckeditor").Include(
+                      "~/Scripts/ckeditor/ckeditor.js"));
+        }
+    }
+}
